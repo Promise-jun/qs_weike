f905047b387a6b984e7d0e6cbf4d017e85b7a95b
panjun <1022988217@qq.com>
2018-05-23 18:02:15 +0800

    20180523

---
+++ js/pdlist1.js
@@ -0,0 +1,80 @@
+var pdlist1 = [
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/icon_share_bg@2x.png",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	},
+	{
+		"img": "./images/bg.jpg",
+		"title": "原来你也在这里",
+		"author": "李四川",
+		"num": "220"
+	}
+];
